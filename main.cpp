5fb618ffdfb2a483792479d3cf5df17b24fa1a27
Zayed Khan <zayedkh02@gmail.com>
2024-09-12 14:11:59 +0100

    Added functions for setup, draw, input and logic

--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 using namespace std;
+bool gameOver;
+
+void setup();
+void draw();
+void input();
+void logic();
 
 int main()
 {
-
+    setup();
+    while (!gameOver)
+    {
+        draw();
+        input();
+        logic();
+    }
 
 
     return 0;
+}
+
+void setup()
+{
+    gameOver = false;
+}
+
+void draw()
+{
+    
+
+}
+
+void input()
+{
+}
+
+void logic()
+{
 }
